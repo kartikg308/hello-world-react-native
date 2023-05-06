677032c033ccb5eb247a9aa17757293ffa595a9f
Kartik <kgarg@practically.com>
2023-05-06 11:51:12 +0530

    hello world

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <Text>My name is Kartik</Text>
+      <Text>My First React Native App</Text>
     </View>
   );
 }
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
   },
 });
